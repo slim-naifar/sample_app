36a47d5d2dc405e612c47c3da7179a0a86ee6a01
Slim <naifarsalim@gamil.com>
2013-06-10 04:57:11 +0100

    chap 3 termine

--- Gemfile
+++ Gemfile
@@ -7,6 +7,13 @@ gem 'rails', '4.0.0.rc1'
 group :development do
   gem 'sqlite3'
   gem 'rspec-rails', '2.11.0'
+  gem 'spork', '0.9.2'
+end
+
+group :development, :test do
+  gem 'guard-rspec', '1.2.1'
+  gem 'guard-spork', '0.3.2'
+
 end
 
 group :production do
@@ -15,6 +22,10 @@ end
 
 group :test do
   gem 'capybara', '1.1.2'
+  gem 'rb-fchange', '0.0.5'
+  gem 'rb-notifu', '0.0.4'
+  gem 'win32console', '1.3.0'
+
 end
 
 # Use SCSS for stylesheets
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "static_pages" do
+
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home Page" do
  
     it "should habe the content 'Sample App'" do
@@ -13,7 +16,7 @@ describe "static_pages" do
  	it "should have the right title" do
  		visit '/static_pages/home'
  		page.should have_selector('title',
- 			                        :text => "Ruby on Rails Tutorial Sample App | Home")
+ 			                        :text => "#{base_title} | Home")
  		end
   end
  
@@ -28,7 +31,7 @@ describe "static_pages" do
  	it "should have the right title" do
  		visit '/static_pages/help'
  		page.should have_selector('title',
- 			                        :text => "Ruby on Rails Tutorial Sample App | Help")
+ 			                        :text => "#{base_title} | Help")
  		end
   end
 
@@ -42,10 +45,22 @@ describe "static_pages" do
 it "should have the right title" do
  		visit '/static_pages/about'
  		page.should have_selector('title',
- 			                        :text => "Ruby on Rails Tutorial Sample App | About Us")
+ 			                        :text => "#{base_title} | About Us")
  		end
 
   	end
 
+ describe "Contact Page" do
+ 	it "should have the content 'Contact'" do
+ 		visit '/static_pages/contact'
+ 		page.should have_selector('h1',
+ 			                       :text => "Contact")
+ 	end
 
+ 	it "should have the right title" do
+ 		visit '/static_pages/contact'
+ 		page.should have_selector('title',
+ 			                       :text => "#{base_title} | Contact")
+ 	end
+ end
 end
